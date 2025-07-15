c2848f0a0a3a4ff720fcd2ad74ae090249bf755d
Himanshu Personal <singh.himanshu04051999@gmail.com>
2025-07-15 22:00:53 +0530

    create function of add

---
+++ tdd_assesment/lib/tdd_assesment.dart
@@ -0,0 +1,30 @@
+int add(String numbers) {
+  if (numbers.isEmpty) return 0;
+   String delimiter = ',';
+
+  if (numbers.startsWith('//')) {
+    final delimiterEnd = numbers.indexOf('\n');
+    delimiter = numbers.substring(2, delimiterEnd);
+    numbers = numbers.substring(delimiterEnd + 1);
+  }
+    numbers = numbers.replaceAll('\n', delimiter);
+    List<String> parts = numbers.split(delimiter);
+
+  List<int> negatives = [];
+  int sum = 0;
+
+  for (var part in parts) {
+    int num = int.parse(part);
+    if (num < 0) {
+      negatives.add(num);
+    } else if (num <= 1000) {
+      sum += num;
+    }
+  }
+
+   if (negatives.isNotEmpty) {
+    throw Exception("negative numbers not allowed ${negatives.join(',')}");
+  }
+
+  return sum;
+}
